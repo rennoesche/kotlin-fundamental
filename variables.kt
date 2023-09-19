4c318506cba4b05b8c148c631a045f9a51801aa0
Rafi Nur Latif <87075107+rennoesche@users.noreply.github.com>
2023-09-20 00:36:29 +0700

    Create variables.kt
    var/val type

---
+++ variables.kt
@@ -0,0 +1,8 @@
+fun variables() {
+  var changeablevariables = "You can change this"
+  changeablevariables = "changed into this"
+  val unchangeablevar = "You cannot change this"
+  // unchangeablevar = "if uncomment this, will get error"
+  println(changeablevariables)
+  println(unchangeablevar)
+}
