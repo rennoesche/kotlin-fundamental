fc654a5df7e9dcfd7f02e75f7a25fe87d6ee4b78
Rafi Nur Latif <87075107+rennoesche@users.noreply.github.com>
2023-09-20 18:31:03 +0700

    Create str-template.kt
    ya begitulah

---
+++ str-template.kt
@@ -0,0 +1,8 @@
+fun main() {
+    val namaku = "Lorem ipsum"
+    val slogan = "David disini"
+    var uangku = 1000000
+
+    println("Halo $namaku, $slogan")
+    println("${ if (uangku>=100000) "Agar silaturahmi tidak terputus,\nboleh dong pinjem seratus" else "..."}")
+}
